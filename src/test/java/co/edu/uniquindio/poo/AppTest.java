e529c543f42a8a680e56618aa06ac400ad565b79
brahian <brahians.mosquerav@uqvirtual.edu.co>
2024-02-15 23:43:31 -0500

    pom configured

--- src/test/java/co/edu/uniquindio/poo/AppTest.java
+++ src/test/java/co/edu/uniquindio/poo/AppTest.java
@@ -1,8 +1,8 @@
 package co.edu.uniquindio.poo;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
